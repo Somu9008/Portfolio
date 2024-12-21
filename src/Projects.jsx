f9c953413d5b957d186d920b657cbba87bd83745
somu9008 <sharathg9008@gmail.comm>
2024-12-21 08:00:20 +0530

    file added

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -12,7 +12,9 @@ export default function Projects() {
       <div className="allProjects">
         <Project
           title={"Airbnb clone Project using MVC"}
-          img={"/src/assets/Screenshot 2024-12-18 140135.png"}
+          img={
+            "https://res.cloudinary.com/dcqvebhun/image/upload/v1734747824/Screenshot_2024-12-18_140135_fefu5b.png"
+          }
           link={"https://wonderlust-project-3.onrender.com/listings"}
           desc={
             "in this project using MVC pattern ,it has fetures like airbn website ,login and signUp functionality and listings ,add reviews and rating ,add new listing ,update listings , upload image in this project useing cloudinary to store files and users can filter by category and searching functionality"
@@ -20,7 +22,9 @@ export default function Projects() {
           tech={"MongoDb,Express.js,Node.js,Ejs"}
         />
         <Project
-          img={"/src/assets/Screenshot 2024-12-18 151221.png"}
+          img={
+            "https://res.cloudinary.com/dcqvebhun/image/upload/v1734747822/Screenshot_2024-12-18_151221_n4hhq9.png"
+          }
           link={"https://shopwell-78by.onrender.com/shop"}
           title={"ecommerse-website using MVC"}
           desc={
@@ -30,7 +34,9 @@ export default function Projects() {
         />
         <Project
           title={"Simon-Says-Game"}
-          img={"/src/assets/Screenshot 2024-12-18 160756.png"}
+          img={
+            "https://res.cloudinary.com/dcqvebhun/image/upload/v1734747819/Screenshot_2024-12-18_160756_cx2fmy.png"
+          }
           link={"https://simon-says-game-dhyi.onrender.com/"}
           desc={
             "Simon Game using HTML, CSS, and JavaScript. In a Simon game, if the player succeeds, the series becomes progressively longer and more complex. Once the user is unable to repeat the designated order of the series at any point, the game is over"
@@ -39,7 +45,9 @@ export default function Projects() {
         />
         <Project
           title={" Find My City Weather Condition"}
-          img={"/src/assets/Screenshot 2024-12-18 164617.png"}
+          img={
+            "https://res.cloudinary.com/dcqvebhun/image/upload/v1734747821/Screenshot_2024-12-18_164617_g3xnbt.png"
+          }
           link={"https://findmyweathercity.netlify.app/"}
           desc={
             " This is the weather app using ReactJs Project. Here we will get the current Weather data From the OpenWeatherMap API . display the weather data like Temperature ,Weather condition ,humidity and wind Speed according to the city"
@@ -49,7 +57,9 @@ export default function Projects() {
         <Project
           title={"e-commerse-website using React Js"}
           link={"https://myshop-ecommerse.netlify.app/shop"}
-          img={"/src/assets/Screenshot 2024-12-18 164913.png"}
+          img={
+            "https://res.cloudinary.com/dcqvebhun/image/upload/v1734747823/Screenshot_2024-12-18_164913_s8hrvk.png"
+          }
           desc={
             "In this frontend eCommerce website Project using ReactJs ,user can explore product,filter and short the produce And user can addthe product in cart by selecting product variant like size. user can also order the product by providing the delivery Address and using any paymentmethod either cash on delivery or online payment"
           }
@@ -58,7 +68,9 @@ export default function Projects() {
         <Project
           title={"University website"}
           link={"https://studenteducationweb.netlify.app/"}
-          img={"/src/assets/Screenshot 2024-12-18 170819.png"}
+          img={
+            "https://res.cloudinary.com/dcqvebhun/image/upload/v1734747831/Screenshot_2024-12-18_170819_dfmhhh.png"
+          }
           desc={
             "this project done by using React this is the landing page of the college or university website, this will be the completely responsive and added react-smoth-scroll effect,slider,,and working contact form on this website ,you can receive the contact form enquireires on your email address "
           }
